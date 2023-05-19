b2522207ba90fc0733884779e76c216fb4760b53
Tran Luan <91442498+luandev12@users.noreply.github.com>
2023-05-19 15:32:59 +0700

    v1.2 cart validate

--- extensions/carts/src/index.ts
+++ extensions/carts/src/index.ts
@@ -1,18 +1,41 @@
-import {
-  InputQuery,
-  FunctionResult,
-  FunctionError,
-} from "../generated/api";
+import { InputQuery, FunctionResult, FunctionError } from "../generated/api";
 
-export default (input: InputQuery): FunctionResult => {
-  const errors: FunctionError[] = input.cart.lines
-    .filter(({ quantity }) => quantity > 1)
-    .map(() => ({
-      localizedMessage: "Not possible to order more than one of each",
-      target: "cart",
-    }));
+export default (input: any): any => {
+  const error = {
+    localizedMessage:
+      "There is an order maximum of $1,000 for customers without established order history",
+    target: "cart",
+  };
 
-  return {
-    errors
+  const errorVN = {
+    localizedMessage: "Viet Nam Not Order",
+    target: "cart",
+  };
+
+  // Parse the decimal (serialized as a string) into a float.
+  const orderSubtotal = parseFloat(input.cart.cost.subtotalAmount.amount);
+  const errors: any = [];
+
+  // Orders with subtotals greater than $1,000 are available only to established customers.
+  console.log(orderSubtotal, "total cost");
+
+  const country = input.localization.country.isoCode;
+
+  if (orderSubtotal > 1000.0) {
+    if (input.cart.buyerIdentity && input.cart.buyerIdentity.customer) {
+      // If the customer has ordered less than 5 times in the past,
+      // then treat them as a new customer.
+      if (input.cart.buyerIdentity.customer.numberOfOrders < 5) {
+        errors.push(error);
+      }
+    } else {
+      errors.push(error);
+    }
   }
+
+  if (country == "VN" && !errors[0]) {
+    errors.push(errorVN);
+  }
+
+  return { errors };
 };
